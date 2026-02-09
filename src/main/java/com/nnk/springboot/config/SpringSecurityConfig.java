6a13ea3a72eb47fc18217001942e19c001be2524
nocivecreative <nocivecreative@gmail.com>
2026-02-09 22:45:32 +0100

    min security

---
+++ src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
@@ -0,0 +1,87 @@
+package com.nnk.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nnk.springboot.security.CustomUserDetailsService;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+
+        private final CustomUserDetailsService customUserDetailsService;
+
+        /**
+         * Configure la filter chain de Spring Security
+         * 
+         * Défini les règles d'autorisation:
+         * - Ouvre toutes les requetes à /user/** sans autorisation (signup, signin,..)
+         * - exige une authentification avec un role USER pour les endpoints /profil/**
+         * - Exige une authentification pour toutes les autres requetes
+         * 
+         * Active le login via formulaire et l'authentification HTTP Basique avec les
+         * paramètres par défaut.
+         * 
+         * @param http l'objet {@link HttpSecurity} à configurer
+         * @return les filters configurés {@link SecurityFilterChain}
+         * @throws Exception si une erreur se produit pendant la configuration
+         */
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/", "/login", "/signup", "/css/**", "/assets/**")
+                                                .permitAll()
+                                                .anyRequest().hasRole("USER"))
+                                .sessionManagement(session -> session
+                                                .sessionFixation().migrateSession() // crée un nouvel ID de session -
+                                                                                    // Invalide l'ancienne
+                                                                                    // session
+                                                .maximumSessions(1)) // une seule session active à la fois (Empêche le
+                                                                     // multi-login)
+                                .formLogin(form -> form
+                                                // .loginPage("/login") // GET
+                                                .loginProcessingUrl("/login") // POST (important)
+                                                .defaultSuccessUrl("/bidList/list", true)
+                                                // .failureUrl("/login?error=true")
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .logoutUrl("/logout")
+                                                .logoutSuccessUrl("/login?logout=true")
+                                                .invalidateHttpSession(true)
+                                                .deleteCookies("JSESSIONID")
+                                                .permitAll());
+
+                return http.build();
+        }
+
+        /**
+         * Provides a BCrypt password encoder bean for secure password hashing.
+         * 
+         * @return a new instance of {@link BCryptPasswordEncoder}
+         */
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager(HttpSecurity http,
+                        BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+                AuthenticationManagerBuilder authenticationManagerBuilder = http
+                                .getSharedObject(AuthenticationManagerBuilder.class);
+                authenticationManagerBuilder.userDetailsService(customUserDetailsService)
+                                .passwordEncoder(bCryptPasswordEncoder);
+                return authenticationManagerBuilder.build();
+        }
+
+}
---
+++ src/main/java/com/nnk/springboot/security/SecurityUser.java
@@ -0,0 +1,39 @@
+package com.nnk.springboot.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.nnk.springboot.domain.User;
+
+import lombok.Getter;
+
+/**
+ * Cette Class est crée uniquement pour différencier le type User de PayMyBuddy
+ * du type User de spring security et éviter la confusion
+ * (org.springframework.security.core.userdetails.User)
+ * 
+ * Il n'est pas prévu d'avoir des rôles en base, donc on hardcode un role "USER"
+ */
+@Getter
+public class SecurityUser implements UserDetails {
+
+    private final Integer id;
+    private final String username;
+    private final String role;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public SecurityUser(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.role = user.getRole();
+        this.password = user.getPassword();
+        // this.authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        this.authorities = List.of(new SimpleGrantedAuthority("ROLE_" + this.role));
+    }
+
+}
